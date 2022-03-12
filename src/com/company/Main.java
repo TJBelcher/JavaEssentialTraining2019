f89d5847e9b8c8f2033bb3d6d1151a0cbcc697a4
Thomas Belcher <tbelcher@verizon.net>
2022-03-12 10:47:05 -0500

    Final commit for 04_04

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,9 @@ public class Main {
         int result = 12 + 22;
         String answer = "The answer is " + result;
         System.out.println(answer);
+        
+        String howMany = String.valueOf(20)+ 10;
+        System.out.println(howMany);
+
     }
 }
